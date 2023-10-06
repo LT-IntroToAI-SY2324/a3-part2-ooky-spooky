6c29beddd1091068d44c000064f44c76f471c5c1
Catherine Markowska <cimarkowska@cps.edu>
2023-10-06 11:20:33 -0500

    began writing a database for horror games

---
+++ database.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple
+
+games_db: List[Tuple[str, str, int, List[str]]] = [
+    ### Here are two of my additions
+        (
+            "Amnesia, the Dark Descent", # title 
+            "Frictional Games", # developer
+            2010, # year released
+            "Alexander of Brennenburg", # villian 
+            [
+                "Alois", # characters 
+                "Basile",
+                "Daniel",
+                "Justine Florbelle",
+                "Malo",
+                "Alexander of Brennenburg"
+            ],
+        ),
+    )
+]
