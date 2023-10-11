bfb7c6e6217ab8cfe541170fd47c5f50f0c1d609
Catherine Markowska <cimarkowska@cps.edu>
2023-10-11 10:57:01 -0500

    began working on asserts

---
+++ asserts.py
@@ -0,0 +1,5 @@
+from database import games_db
+from database import books_db
+from database import movies_db
+from match import match
+from typing import List, Tuple, Callable, Any
--- database.py
+++ database.py
@@ -51,7 +51,7 @@ games_db: List[Tuple[str, str, int, List[str]]] = [
                 "Chris Redfield",
                 "Mia Winters",
                 "Rosemary Winters",
-                "Alcina Dimitrescu"
+                "Alcina Dimitrescu",
                 "The Duke",
                 "Mother Miranda",
                 "Salvaore Moeau",
@@ -82,171 +82,171 @@ games_db: List[Tuple[str, str, int, List[str]]] = [
         ),
 ]
 
-books_db: List[Tuple[str, str, int, List[str]]] = [ #### WE HAVE AN ERROR SOMEWHERE -- [ AND ( NOT CLOSED, remember to be on the lookout for it :3 - Cat
-    (
-        "it",  # title
-        "stephen king",  # author
-        1986,  # year released
-        "pennywise", #main villian
-        [
-            "richie tozier",
-            "eddie kasbrak",
-            "ben hanscom",
-            "mike hanlon",
-            "dick hallorann",
-        ],  # characters
-    ),
-    (
-        "dracula",
-        "bram stoker",
-        1897,
-        "count dracula",
-        [
-            "mina harker",
-            "jonathan harker",
-            "arthur holmwood",
-            "renfield",
-            "dr. john seward",
-        ],
-    ),
-    (
-        "carrie",
-        "stephen king",
-        1974,
-        "carrie white",
-        [
-            "margaret white",
-            "sue snell",
-            "rita desjardin",
-            "chris hargensen",
-        ],
-    ),
-    (
-        "frankenstein",
-        "mary shelley",
-        1818, # oldest book in this database
-        "frankenstein",
-        [
-            "victor frankenstein",
-            "cxaptin robert walton",
-            "elizabeth lavenza",
-            "dr. henry clerval",
-        ],
-    ),
-    (
-        "bird box",
-        "josh malerman",
-        2014, 
-        "malevolent beings",
-        [
-            "tom",
-            "jules",
-            "victor (dog)",
-            "felix",
-            "malorie",
-            "shannon",
-        ],
-    ),
-    (
-        "rosemary's baby",
-        "ira levin",
-        1967,
-        "the castevets",
-        [
-            "rosemary woodhouse",
-            "roman castevet",
-            "guy woodhouse",
-            "minnie castevet",
-            "the devil",
-            "mr. nicklas",
-        ],
-    ),
-    (
-        "the woman in black",
-        "susan hill",
-        1983,
-        "woman in black",
-        [
-            "arthur kipps",
-            "alice drablow",
-            "mr. jerome",
-            "mr. daily",
-            "keckwick",
-        ],
-    ),
-    (
-        "the hunger",
-        "alma katsu"
-        2018,
-        "james reed",
-        [
-            "elitha donner",
-            "tamsen donner",
-            "charles stanton",
-            "mary graves",
-            "jean-baptiste trudeau",
-        ],
-    ),
-    (
-        "ring",
-        "koji suzuki",
-        1991,
-        "sadako yamamura",
-        [
-            "kazuyuki asakawa",
-            "ryuji takayama",
-            "shizuki asakawa",
-            "yoko asakawa",
-        ],
-    ),
-    (
-        "house of leaves",
-        "mark z. danielewski",
-        2000,
-        "unnamed monster",
-        [
-            "johnny truant",
-            "zampano",
-            "lude",
-            "will navidson",
-        ],
-    ),
-    (
-        "a head full of ghosts",
-        "paul g. tremblay",
-        2015,
-        "marjorie",
-        [
-            "meredith (merry) barrett",
-            "marjorie berrett",
-            "john barrett",
-            "sarah barrett",
-        ],
-    ),
-    (
-        "come closer",
-        "sara gran",
-        2003,
-        "naamah",
-        [
-            "amanda",
-            "adam",
-        ],
-    ),
-    (
-        "no24r2",
-        "joe hill, gabriel rodriguez",
-        2013,
-        "charles manx",
-        
-        [
+books_db: List[Tuple[str, str, int, List[str]]] = [   #### WE HAVE AN ERROR SOMEWHERE -- [ AND ( NOT CLOSED, remember to be on the lookout for it :3 - Cat
+        (
+            "it",  # title
+            "stephen king",  # author
+            1986,  # year released
+            "pennywise", #main villian
+            [
+                "richie tozier",
+                "eddie kasbrak",
+                "ben hanscom",
+                "mike hanlon",
+                "dick hallorann",
+            ],  # characters
+        ),
+        (
+            "dracula",
+            "bram stoker",
+            1897,
+            "count dracula",
+            [
+                "mina harker",
+                "jonathan harker",
+                "arthur holmwood",
+                "renfield",
+                "dr. john seward",
+            ],
+        ),
+        (
+            "carrie",
+            "stephen king",
+            1974,
+            "carrie white",
+            [
+                "margaret white",
+                "sue snell",
+                "rita desjardin",
+                "chris hargensen",
+            ],
+        ),
+        (
+            "frankenstein",
+            "mary shelley",
+            1818, # oldest book in this database
+            "frankenstein",
+            [
+                "victor frankenstein",
+                "cxaptin robert walton",
+                "elizabeth lavenza",
+                "dr. henry clerval",
+            ],
+        ),
+        (
+            "bird box",
+            "josh malerman",
+            2014, 
+            "malevolent beings",
+            [
+                "tom",
+                "jules",
+                "victor (dog)",
+                "felix",
+                "malorie",
+                "shannon",
+            ],
+        ),
+        (
+            "rosemary's baby",
+            "ira levin",
+            1967,
+            "the castevets",
+            [
+                "rosemary woodhouse",
+                "roman castevet",
+                "guy woodhouse",
+                "minnie castevet",
+                "the devil",
+                "mr. nicklas",
+            ],
+        ),
+        (
+            "the woman in black",
+            "susan hill",
+            1983,
+            "woman in black",
+            [
+                "arthur kipps",
+                "alice drablow",
+                "mr. jerome",
+                "mr. daily",
+                "keckwick",
+            ],
+        ),
+        (
+            "the hunger",
+            "alma katsu",
+            2018,
+            "james reed",
+            [
+                "elitha donner",
+                "tamsen donner",
+                "charles stanton",
+                "mary graves",
+                "jean-baptiste trudeau",
+            ],
+        ),
+        (
+            "ring",
+            "koji suzuki",
+            1991,
+            "sadako yamamura",
+            [
+                "kazuyuki asakawa",
+                "ryuji takayama",
+                "shizuki asakawa",
+                "yoko asakawa",
+            ],
+        ),
+        (
+            "house of leaves",
+            "mark z. danielewski",
+            2000,
+            "unnamed monster",
+            [
+                "johnny truant",
+                "zampano",
+                "lude",
+                "will navidson",
+            ],
+        ),
+        (
+            "a head full of ghosts",
+            "paul g. tremblay",
+            2015,
+            "marjorie",
+            [
+                "meredith (merry) barrett",
+                "marjorie berrett",
+                "john barrett",
+                "sarah barrett",
+            ],
+        ),
+        (
+            "come closer",
+            "sara gran",
+            2003,
+            "naamah",
+            [
+                "amanda",
+                "adam",
+            ],
+        ),
+        (
+            "no24r2",
+            "joe hill, gabriel rodriguez",
+            2013,
             "charles manx",
-            "vic mcqueen",
-            "linda mcqueen",
-            "chris mcqueen",
-            "lou carmody",
-        ],
-    ),
+        
+            [
+                "charles manx",
+                "vic mcqueen",
+                "linda mcqueen",
+                "chris mcqueen",
+                "lou carmody",
+            ],
+        ),
 ]
 
 movies_db: List[Tuple[str, str, int, List[str]]] = [
